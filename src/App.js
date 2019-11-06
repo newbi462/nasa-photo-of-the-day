12aa91da6a24860ef8886e7fe966b472d1c28fc2
newbi462 <isthisemailfree@gmail.com>
2019-11-06 16:31:10 -0500

    API start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
+
 function App() {
+  //
+  const [apiData, setSpiData] = useState([])
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${page}&api_key=DEMO_KEY`)
+    .then(response => {
+      console.log(response.data.photos)
+      //setSpiData(response.data.message)
+    })
+    .catch( error => {
+      console.log("sorry no dogs", error)
+    })
+  },[])
+
+
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun 🚀! Still
       </p>
     </div>
   );
